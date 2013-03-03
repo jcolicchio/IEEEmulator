61ccf5832ab178600484180f6a51791ea72fc9db
Zhansaya Abdikarimova <zhansaya@umail.ucsb.edu>
2013-03-03 15:18:13 -0800

    added files from intellij

--- GameLogic.java
+++ GameLogic.java
@@ -1,24 +1,79 @@
-public class GameLogic {
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhansaya
+ * Date: 2/28/13
+ * Time: 7:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
 
-	public int A;
-	public int X;
-	public int Y;
-	public int PC;
 
-	public Boolean C;
-	public Boolean Z;
+public class GameLogic implements Runnable {
+    public int A;
+    public int X;
+    public int Y;
+    public int PC;
 
-	public int[] memory;
+    public Boolean C;
+    public Boolean Z;
 
-	public GameLogic() {
-		System.out.println("a new game logic instance was created");
+    public int[] memory;
+
+
+    int loc;
+
+
+
+    public GameLogic() {
+        System.out.println("a new game logic instance was created");
+        memory = new int[5];
+        memory[0]=0xa9;
+        memory[1]=0x05;
+
+
+
+
+    }
+
+    //let's use a thread for the update cycle
+    //when you call gameLogic.start(); it should make the thread and start it running
+    public void start(){
+        (new Thread(new GameLogic())).start();
+    }
+
+    public void run() {
+
+        loc = 0;
+
+        while(memory[loc] != 0x00 && loc < memory.length){
+            switch (memory[loc]){
+                case 0xa9:{
+	                LDA();
+
+
+
+                }
+                loc++;
+            }
+
+        }
+        System.out.println(A);
+
+    }
+
+	public void LDA(){
+		A = memory[++loc];
+		//System.out.print(A);
+
+
 	}
 
-	//let's use a thread for the update cycle
-	//when you call gameLogic.start(); it should make the thread and start it running
+	public void LDX(){
+		X = memory[++loc];
 
-	public void start() {
-		
 	}
+	public void LDY(){
+		Y = memory[++loc];
+	}
+
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.Integer.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+	// write your code here
 
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.start();
+
+
+
+
+    }
 }
